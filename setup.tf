5b3a2e516302e4b578c23da7bb9eedae07b67b1a
Jacob Cook <61634762+jcook135@users.noreply.github.com>
2025-05-08 18:13:04 -0400

    Create setup.tf

---
+++ setup.tf
@@ -0,0 +1,32 @@
+variable "vm_count" {
+  default = 2
+}
+
+resource "azurerm_linux_virtual_machine" "kali" {
+  count               = var.vm_count
+  name                = "student-kali-${count.index + 1}"
+  resource_group_name = azurerm_resource_group.kali.name
+  location            = azurerm_resource_group.kali.location
+  size                = "Standard_B2s"
+
+  admin_username      = "ita"
+  admin_password      = "820ITAcademy"
+
+  network_interface_ids = [
+    azurerm_network_interface.kali[count.index].id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "kali-linux"
+    offer     = "kali"
+    sku       = "kali"
+    version   = "latest"
+  }
+
+  custom_data = filebase64("cloud-init-kali.yaml") # Cloud-init script
+}
