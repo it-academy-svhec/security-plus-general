2346d9b8b7fb4ca5339ec7e75d052aebfecd795b
Jacob Cook <61634762+jcook135@users.noreply.github.com>
2025-05-27 19:52:41 -0400

    Update wordpress-server.sh

--- wordpress-server.sh
+++ wordpress-server.sh
@@ -1,1 +1,15 @@
-
+sudo apt update
+sudo apt install apache2 \
+                 ghostscript \
+                 libapache2-mod-php \
+                 mysql-server \
+                 php \
+                 php-bcmath \
+                 php-curl \
+                 php-imagick \
+                 php-intl \
+                 php-json \
+                 php-mbstring \
+                 php-mysql \
+                 php-xml \
+                 php-zip
